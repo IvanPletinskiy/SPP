9017bffe66a33202eee1c4d414f2cd3032adec0b
Ivan Pletinskij <ivan@luno.com>
2022-04-20 23:32:11 +0300

    Add React component for AccountsList

--- frontend/src/components/AccountsList.js
+++ frontend/src/components/AccountsList.js
@@ -1,23 +1,32 @@
 import React from "react";
+import {getCurrencyNameById, isFiat} from "../utils/CryptoUtils";
 
 class AccountsList extends React.Component {
 
-    componentDidMount() {
-        super.componentDidMount();
-    }
-
     render() {
-        return (
-            <body className="page-landing">
-            <PageTop>
-                <TopPlayers topPlayersData={this.props.topPlayersData} />
-            </PageTop>
-            <PageBottom>
-                <RecentGames recentGamesData=    {this.props.recentGamesData}/>
-            </PageBottom>
-            </body>
-        );
+        return this.props.accounts.map(account => {
+                var buttons;
+                if (isFiat(account.fk_cc_id)) {
+                    buttons = <div>
+                        <button>Пополнить</button>
+                        <button>Вывести</button>
+                    </div>
+                } else {
+                    buttons = <div>
+                        <button>Купить</button>
+                    </div>
+                }
+                return (
+                    <div>
+                        <h5>{account.ca_number}</h5>
+                        <h6>{getCurrencyNameById(account.fk_cc_id)}</h6>
+                        <h6>{account.ca_amount}</h6>
+                        <div>{buttons}</div>
+                    </div>
+                )
+            }
+        )
     }
 }
 
-export default LandingPageBox;
+export default AccountsList;
