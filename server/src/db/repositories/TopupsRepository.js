6169c4fd5957767378b738a69564f2b9b4a65b74
Maksim Malenho <maks.malengo@gmail.com>
2022-04-26 21:06:52 +0300

    fix smth

--- server/src/db/repositories/TopupsRepository.js
+++ server/src/db/repositories/TopupsRepository.js
@@ -9,14 +9,12 @@ const pool = new Pool({
 export class TopupsRepository {
     async addTopup(topup) {
         const to_fiat_amount = Number(topup.to_fiat_amount)
-        const to_datetime = Number(topup.to_datetime)
         const fk_ca_id = Number(topup.fk_ca_id)
         console.log("Topup:")
         console.log(to_fiat_amount)
-        console.log(to_datetime)
         console.log(fk_ca_id)
-        const text = 'INSERT INTO "Topup" (to_fiat_amount, to_datetime, fk_ca_id) VALUES ($1, $2, $3)'
-        const values = [to_fiat_amount, to_datetime, fk_ca_id];
+        const text = 'INSERT INTO "Topup" (to_fiat_amount, to_datetime, fk_ac_id) VALUES ($1, $2, $3)'
+        const values = [to_fiat_amount, topup.to_datetime, fk_ca_id];
         const res = await pool.query(text, values)
         return res
     }
